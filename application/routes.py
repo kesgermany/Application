291b272fdd114428ba6f7ef6ce16d9eb1111e3ee
Kes Germany <kesgermany@yahoo.com>
2023-08-15 19:05:18 +0100

    EG: Form commit

--- application/routes.py
+++ application/routes.py
@@ -3,12 +3,18 @@ from application.models import Products
 from application.models import Customer
 from flask import Flask, render_template, request
 from flask import jsonify
+from form import ContactForm
 
 
 @app.route('/customers', methods=['GET'])
 def customers():
     customers = Customer.query.all()
     return jsonify([customer.to_json() for customer in customers])
+
+@app.route('/contactform')
+def contactform():
+    form = ContactForm()
+    return render_template('contactform.html', form=form)
 
 
 
---
+++ form.py
@@ -0,0 +1,17 @@
+from typing import Any
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, IntegerField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Length
+
+class ContactForm(FlaskForm):
+    first_name = StringField('First Name', validators=[
+        DataRequired(),
+        Length(min=2, max=30)
+    ])
+    last_name = StringField('Last Name')
+    message = StringField('Type your message', validators=[
+        DataRequired(),
+        Length(min=10, max=100)
+    ])
+    submit = SubmitField('Send')
